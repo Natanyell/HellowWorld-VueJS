012df89f87568fe45dbc99f6d487bc4842eb6d59
Natanyell <rnatanyell@gmail.com>
2024-07-03 21:51:39 -0300

    primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Bem-vindo ao seu aplicativo Vue.js"/>
 </template>
 
 <script>
